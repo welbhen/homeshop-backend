d50dffe98a11626c835e9ddcc454db62514c22d4
Welberth <welberth_henrique@hotmail.com>
2022-04-14 15:04:29 -0300

    Index.js changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
+//const cookieParser = require('cookie-parser');
 //const bodyParser = require('body-parser');
 //const mongoose = require('mongoose');
 //const MongoStore = require('connect-mongo');
@@ -77,68 +77,6 @@ require('./database');
     app.use('/', require('./routes/main.routes'));
     app.use('/admin', require('./routes/admin.routes'));
     app.use('/user', require('./routes/user.routes'));
-
-/* #################### NEW - for test purposes*/
-app.post('/login-TEST', async (req, res, next) => {
-    console.log("Inside POST route /user/login, Req.body: " + req.body);
-    console.log("User: " + req.body.email);
-    console.log("Password: " + req.body.password);
-    console.log("_____________________________________");
-    try{
-        passport.authenticate('local', (err, user, info) => {
-            if(!user) {
-                res.json({
-                    error: true,
-                    message: "No user found with this e-mail!"
-                });
-            }else {
-                req.logIn(user, err => {
-                    if(err){
-                        console.log("Error inside req.logIn on user.routes: " + err);
-                        return next(err);
-                        /*
-                        res.json({
-                            error: true,
-                            message: "An error occured!"
-                        });
-                        */
-                    }
-                    //res.cookie('userid', user.id, { maxAge: 1 * 60 * 60 * 1000 });  // Expires in and hour
-                    console.log("Inside 'post' /user/login -> passport.authenticate");
-                    console.log("user.id: " + user.id);
-                    console.log("info: ", info);
-                    console.log("user: ", user);
-                    console.log("req.user: ", req.user);
-                    console.log("isAthenticated: " + req.isAuthenticated());
-                    console.log("____________________________________________________");
-                    
-                    res.json({
-                        error: false,
-                        message: "User logged-in with success!",
-                        user
-                    });
-                    //next();
-                    
-                    //console.log("Inside passport.authenticate, req.user: " + req.user);
-                });
-            }
-        })(req, res, next);
-        /*
-        console.log("After passport.authenticate");
-        console.log("#################### req.user: ", req.user);
-        console.log("#################### req.email: ", req.email);
-        //console.log("#################### res: ", res);
-        console.log("____________________________________________________");
-        */
-    }catch(err){
-        res.json({
-            error: true,
-            message: err.message
-        });
-    }
-	
-});
-/* ########################## for test purposes*/
 
 // Server:
     app.set('port', process.env.PORT || 8000);
